d467b1bb8b4aaa9cab93f55bee6372665029af35
Caio Rebert <caiorebert070802.cr@gmail.com>
2024-03-05 19:58:35 -0300

    first commit

---
+++ src/main/java/com/imd/qualquercoisa20conto/Qualquercoisa20contoApplication.java
@@ -0,0 +1,13 @@
+package com.imd.qualquercoisa20conto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qualquercoisa20contoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qualquercoisa20contoApplication.class, args);
+	}
+
+}
